0a6a97bb82c04aa16192f25704e62f059fdb010d
Daniel <70107167+Programador-jr@users.noreply.github.com>
2025-12-30 09:02:17 -0300

    Atualizar o dashboard.controller.js

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -2,19 +2,19 @@ const Transaction = require("../models/Transaction");
 const Box = require("../models/Box");
 
 exports.summary = async (req, res) => {
-  const familyId = req.user.familyId;
-
-  const transactions = await Transaction.find({ familyId });
+  const transactions = await Transaction.find({
+    familyId: req.familyId
+  });
 
   let income = 0;
   let expense = 0;
 
   transactions.forEach(t => {
     if (t.type === "income") income += t.value;
-    else if (t.type === "expense") expense += t.value;
+    if (t.type === "expense") expense += t.value;
   });
 
-  const boxes = await Box.find({ familyId });
+  const boxes = await Box.find({ familyId: req.familyId });
 
   res.json({
     income,
